47a7a5527dc5920ed00f09ab113934698dc78d6a
Walid Newaz <mdwalid@gmail.com>
2023-06-10 19:58:49 -0600

    Added favorite selection

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,8 @@ import Root, {
 } from './routes/root'
 import ErrorPage from './error-page'
 import Contact, {
-  loader as contactLoader
+  loader as contactLoader,
+  action as contactAction,
 } from './routes/contact'
 import EditContact, {
   action as editAction,
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         path: "/contacts/:contactId",
         element: <Contact />,
         loader: contactLoader,
+        action: contactAction,
       },
       {
         path: "/contacts/:contactId/edit",
--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -6,15 +6,16 @@ import {
     redirect,
     NavLink,
     useNavigation,
+    useSubmit,
 } from "react-router-dom";
+import { useEffect } from "react";
 import { getContacts, createContact } from "../contacts";
 
 export async function loader({ request }) {
     const url = new URL(request.url);
     const q = url.searchParams.get("q");
     const contacts = await getContacts(q);
-    // const contacts = await getContacts();
-    return { contacts };
+    return { contacts, q };
 }
 
 export async function action() {
@@ -23,8 +24,18 @@ export async function action() {
 }
 
 export default function Root() {
-    const { contacts } = useLoaderData();
+    const { contacts, q } = useLoaderData();
     const navigation = useNavigation();
+    const submit = useSubmit();
+
+    const searching = navigation.location &&
+        new URLSearchParams(navigation.location.search).has(
+            "q"
+        );
+
+    useEffect(() => {
+        document.getElementById("q").value = q;
+    }, [q]);
 
     return (
         <>
@@ -38,11 +49,19 @@ export default function Root() {
                             placeholder="Search"
                             type="search"
                             name="q"
+                            defaultValue={q}
+                            onChange={(event) => {
+                                const isFirstSearch = q == null;
+                                submit(event.currentTarget.form, {
+                                    replace: !isFirstSearch,
+                                });
+                            }}
+                            className={searching ? "loading" : ""}
                         />
                         <div
                             id="search-spinner"
                             aria-hidden
-                            hidden={true}
+                            hidden={!searching}
                         />
                         <div
                             className="sr-only"
